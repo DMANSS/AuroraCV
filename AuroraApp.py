060f2c1b2b1e6d59d4eb71abfea95f77bcab2d79
DMANSS <deadmanss@yandex.ru>
2018-07-02 18:28:07 +0300

    Собираю в один исполнительный файл-сервер и фрейм

--- AuroraApp.py
+++ AuroraApp.py
@@ -1,10 +1,33 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import SSV
-import faceDet
-server_address = ("", 8000)
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+import cgi
+import html
+import FrameVideo
 
-cam = SSV.Videocam()
-cam1 = faceDet()
-cam.detect()
+cam = FrameVideo.Videocam()
+
+form = cgi.FieldStorage()
+text1 = form.getfirst("TEXT_1", "не задано")
+text2 = form.getfirst("TEXT_2", "не задано")
+text1 = html.escape(text1)
+text2 = html.escape(text2)
+
+
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>AuroraClient</title>
+        </head>
+        <body>""")
+
+print("<h1>Client</h1>")
+print("<p>Timeline: {}</p>".format(text1))
+print("<p>Object: {}</p>".format(text2))
+print("<img src='{}'>", img)
+print('<video width="320" height="240" poster="/images/w3html5.gif" controls>')
+print('<source src="movie.mp4" type="video/mp4">')
+print('<source src="movie.ogg" type="video/ogg">')
+print("Your browser does not support the video tag.")
+print("</video>")
+print("""</body>
+        </html>""")
--- FrameVideo.py
+++ FrameVideo.py
@@ -1,29 +1,28 @@
 import cv2
 import sys
-import sqlite3
-import urllib.request
-import urllib.parse
 import time
-import flask
 
 class Videocam():
 
     def __init__(self):
         self.detect()
 
-    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+
 
     def detect(self):
         cap = cv2.VideoCapture(0)
+
         if not cap.isOpened():
             print("Камера не в 0 порту")
             sys.exit()
+
         tracker = cv2.TrackerMedianFlow_create()
+
         ok, frame = cap.read()
         if not ok:
             print('Ошибка чтения видео')
             sys.exit()
+
         bbox = (287, 23, 86, 320)
         bbox = cv2.selectROI(frame, False)
         ok = tracker.init(frame, bbox)
@@ -57,11 +56,29 @@ class Videocam():
 
             # HUD fps
             cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+
+            face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+            eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+
+            image = frame.copy()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                # roi_gray = gray[y:y + h, x:x + w]
+                # roi_color = img[y:y + h, x:x + w]
+                for_eyes = gray[y:y + h, x:x + w]
+                eyes = eye_cascade.detectMultiScale(for_eyes)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(image, (ex + x, ey + y), (x + ex + ew, y + ey + eh), (0, 255, 0), 2)
+
             # Вывод
-            cv2.imshow("Tracking", frame)
+            #cv2.imshow("Tracking", frame)
+            cv2.imshow("Faces", image)
 
             # Выход по клавише esc
             k = cv2.waitKey(1) & 0xff
             if k == 27: break
 
-
+v1 = Videocam()
+v1.detect()
